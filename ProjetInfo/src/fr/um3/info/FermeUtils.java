9d8280763b9900742d954c61dfd749520642e817
orthegavalentin <iravadu@gmail.com>
2022-11-13 16:29:50 +0100

    Changes

--- ProjetInfo/src/fr/um3/info/FermeUtils.java
+++ ProjetInfo/src/fr/um3/info/FermeUtils.java
@@ -2,6 +2,12 @@ package fr.um3.info;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FermeUtils {
     public static BufferedImage resize(BufferedImage img, int newW, int newH) {
@@ -13,5 +19,34 @@ public class FermeUtils {
         g2d.dispose();
 
         return dimg;
+    }
+    public static List<String> generateMapFromTextFile(String filepath)  {
+
+        // list that holds strings of a file
+        List<String> listOfStrings = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.getProperty("user.dir"));
+        sb.append("\\");
+        sb.append(filepath);
+
+       try{ BufferedReader bf = new BufferedReader(new FileReader(sb.toString()));
+
+        // read entire line as string
+        String line = bf.readLine();
+
+        // checking for end of file
+        while (line != null) {
+            listOfStrings.add(line);
+            line = bf.readLine();
+        }
+
+        // closing bufferreader object
+        bf.close();}
+       catch(IOException ex){
+           System.out.println(ex.toString());
+       }
+
+        return listOfStrings;
     }
 }
